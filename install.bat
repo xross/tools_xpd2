06dc587fb931b3b92743876fccb9be1907ec4623
David Norman <davidn@xmos.com>
2011-09-09 13:03:19 +0100

    Adding text to let the user know that full release is an option

--- install.bat
+++ install.bat
@@ -4,6 +4,8 @@ setlocal
 for %%a in (python.exe) do set python_path=%%~dp$PATH:a
 echo Installing to %python_path%
 
-if not exist %python_path%\Lib\site-packages\xpd  mkdir %python_path%\Lib\site-packages\xpd\
+if not exist %python_path%\Lib\site-packages\xpd      mkdir %python_path%\Lib\site-packages\xpd
+if not exist %python_path%\Lib\site-packages\Scripts  mkdir %python_path%\Lib\site-packages\Scripts
+
 xcopy /F/Y scripts\xpd %python_path%\Scripts\xpd
 xcopy /F/Y xpd\*.py %python_path%\Lib\site-packages\xpd\
