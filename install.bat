9ac13194c4fe6e1c695d737b4b1d7531c934cdf8
David Norman <davidn@xmos.com>
2011-09-09 12:46:13 +0100

    Adding file to install xpd into the local python installation

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+
+setlocal
+for %%a in (python.exe) do set python_path=%%~dp$PATH:a
+echo Installing to %python_path%
+
+if not exist %python_path%\Lib\site-packages\xpd  mkdir %python_path%\Lib\site-packages\xpd\
+xcopy /F/Y scripts\xpd %python_path%\Scripts\xpd
+xcopy /F/Y xpd\*.py %python_path%\Lib\site-packages\xpd\
