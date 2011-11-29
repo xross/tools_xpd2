32bae1f7ee5d4e4033811a750d87f2cf9b443e0d
Dave Lacey <david.lacey@xmos.com>
2011-11-29 13:37:45 +0000

    Added some Mac compatibility stuff

--- xpd/xpd_subprocess.py
+++ xpd/xpd_subprocess.py
@@ -3,11 +3,28 @@ import re
 import platform
 import subprocess
 
-use_shell = re.match('.*[W|w]in.*',platform.system())
+if re.match('.*[W|w]in.*',platform.system()):
+    use_shell = True
+    join_args = False
+elif re.match('.*Darwin.*',platform.system()):
+    use_shell = True
+    join_args = True
+else:
+    use_shell = False
+    join_args = False
+
 def Popen(*args, **kwargs):
+    if join_args:
+        args = list(args)
+        args[0] = [' '.join(args[0])]
+
     kwargs['shell'] = use_shell
     return subprocess.Popen(*args,**kwargs)
 
 def call(*args, **kwargs):
+    if join_args:
+        args = list(args)
+        args[0] = [' '.join(args[0])]
+
     kwargs['shell'] = use_shell
     return subprocess.call(*args,**kwargs)
