237743362fecce497ec47981662ff29bcd2882a8
David Lacey <david.lacey@xmos.com>
2012-09-27 10:05:12 +0100

    Update changelog checking

--- xpd/templates.py
+++ xpd/templates.py
@@ -580,3 +580,20 @@ swblock_readme = '''\
 
 <Add description of software block>
 '''
+
+changelog = '''\
+<Insert Repo name> Change Log
+=============================
+
+1.1.0
+-----
+  * Bullet points of features
+  * Another point
+  * ...
+
+1.0.0
+-----
+  * Initial Version
+'''
+
+
