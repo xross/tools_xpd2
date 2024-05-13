297455301b0d52b8c7a4d8a304dee887c5e6c89f
Ross Owen <ross@xmos.com>
2024-05-13 19:43:20 +0100

    - Added xpd_check_sandbox - Started to add implementation of xpd_create_release

--- xpd2.py
+++ xpd2.py
@@ -14,6 +14,7 @@ from optparse import OptionParser
 import sys, os
 from xpd2.xpd_data import Repo, Sandbox
 from pathlib import Path
+from xpd2.xpd_version import Version, VersionParseError
 
 
 common_commands = [
@@ -32,16 +33,40 @@ WIP_commands = [
     ("update", "update %prog to latest version"),
 ]
 
+
 def xpd_check_sandbox(sandbox, options, args):
 
-    #errors = get_multiple_version_errors(sandbox)
-    pass
+    errors = 0
+    warnings = 0
 
+    # TODO Check sandbox for errors here:
+    # - Check if we're tracking a branch the lastest version is present
 
+    # Do some basic checking for now
+    # Check we have the version of a repo we specify in the dependencies
+    for repo in sandbox._repos:
+        for dep in repo.dependencies:
+            dep_repo = sandbox.find_repo_by_name(dep.name)
+            if dep_repo.version:
+                if dep.version:
+                    if dep_repo.version != dep.version:
+                            errors += 1
+                            log_error('%s : %s used by %s' % (dep_repo.name, dep.version, repo.name))
+                            log_info("ERROR: Instead use actual version %s" %(dep_repo.version))
+            else:
+                # TODO properly deal with githash case
+                log_warning(f"{dep_repo} is not on a version but on hash: {dep_repo.current_version_or_githash(short=True)}")
+                warnings += 1
+
+        if repo.behind_upstream and not options.force:
+            log_error(f"{repo.name}: Upstream changes not merged in. Cannot create release")
+            log_error("Try a 'git pull'")
+            errors += 1
+
+    return (errors, warnings)
 
 def xpd_create_release(sandbox, options, args):
 
-    # Check if sandbox has any uncommitted modifications
     local_mod = False
     for r in sandbox._repos:
         if r.has_local_modifications:
@@ -52,8 +77,76 @@ def xpd_create_release(sandbox, options, args):
         log_error("Cannot create release: uncommitted modifications")
         sys.exit(1)
 
+    (errors, warnings) = xpd_check_sandbox(sandbox, options, args)
 
+    if errors or warnings:
+        log_error(f"Fix the {errors + warnings} detected problem(s) before making a release")
+        sys.exit(1)
 
+    if hasattr(options, 'release_type') and options.release_type:
+        rtype = options.release_type
+    else:
+        rtype = None
+        while True:
+            x = input("Enter release type (a=alpha,b=beta,rc=rc,r=release): ")
+            if x in ['a', 'alpha']:
+                rtype = 'alpha'
+            elif x in ['b', 'beta']:
+                rtype = 'beta'
+            elif x in ['rc', 'c']:
+                rtype = 'rc'
+            elif x in ['r']:
+                rtype = ''
+
+            if rtype or rtype == '':
+                break
+            else:
+                # No need to make an error that is logged
+                print(("ERROR: Unknown release type '%s'" % x))
+
+    #TODO handle branched from case?
+
+    # Check for changelog
+    repo = sandbox._repos[0]
+    notes = repo.changelog_entries
+    if not notes and not options.force:
+        log_error("No versions found in CHANGELOG.rst, please update file first")
+        sys.exit(1)
+
+    # Get the version number we want to release
+    (latest_in_changelog, items) = notes[0]
+
+    if hasattr(options, 'release_version') and options.release_version:
+        print('release_version: ' + str(options.release_version))
+        version = Version(version_str=options.release_version)
+    else:
+        latest = repo.latest_full_release
+        if latest:
+            print(("Latest release: %s" % latest.version))
+            latest_version = latest.version
+        else:
+            print("There is no full release yet")
+            latest_version = Version(0, 0, 0, 0)
+
+        print(f"    Next major: {latest_version.major_increment()}")
+        print(f"    Next minor: {latest_version.minor_increment()}")
+        print(f"    Next point: {latest_version.point_increment()}")
+
+        latest_pre = repo.latest_pre_release
+        if latest_pre and (not latest or latest_pre > latest):
+            print(("Latest pre-release: %s" % latest_pre.version))
+
+        version = None
+        while True:
+            x = input(f"Enter version number [{latest_in_changelog}]:")
+            if not x:
+                x = latest_in_changelog
+
+            try:
+                version = Version(version_str=x)
+                break
+            except:
+                log_error("Invalid version number '%s'" % x)
 
 
 def xpd_status(sandbox, options, args):
@@ -120,6 +213,9 @@ def main():
         help="Ignore safety checks",
     )
 
+    optparser.add_option("-t", "--release-type", dest="release_type",
+                         help="release type: release, alpha, beta or rc")
+
     (options, args) = optparser.parse_args()
     if len(args) < 1:
         optparser.error("Please specify a command")
@@ -131,8 +227,6 @@ def main():
         sys.exit(0)
 
     repo_path = Path(os.getcwd())
-
-    print("top level repo:" + str(repo_path))
 
     sandbox = Sandbox(repo_path)
 
